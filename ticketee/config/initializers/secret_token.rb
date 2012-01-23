0b47d1f37d81fabbd965c2fe8b922c09f3e3c968
Chris Larson <krystoff@uw.edu>
2012-01-22 19:03:29 -0800

    ticketee base config, gems updated, test env generated

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'ede89dc43861fe210db0673483afe773e4a4d0b0eef980109b62c423c0eff3f899cbf2b1bc07ff29e415bd9f4507f6c6db77d27e35055600f57bf2303e8fbaee'
