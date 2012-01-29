820c655ef8ea6bae9f60ebcb1a169bdd6f839083
Chris Larson <krystoff@uw.edu>
2012-01-28 22:36:54 -0800

    Created 404 handler

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+  
   def index
     @projects = Project.all
   end
@@ -18,16 +20,13 @@ class ProjectsController < ApplicationController
     end
   end
   
-  def show
-    @project = Project.find(params[:id])
+  def show    
   end
   
-  def edit
-    @project = Project.find(params[:id])
+  def edit    
   end
   
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -38,10 +37,18 @@ class ProjectsController < ApplicationController
   end
   
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
+  
+  private
+    def find_project
+      @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+      " for could not be found."
+      redirect_to projects_path
+    end
   
 end
