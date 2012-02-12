4929b5020eab6516c7056e4a6db7b3370de954a8
Chris Larson <krystoff@uw.edu>
2012-02-12 13:54:35 -0800

    Ch9: protected files

--- ticketee/app/models/ticket.rb
+++ ticketee/app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   has_many :assets
   accepts_nested_attributes_for :assets
+  
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
 end
