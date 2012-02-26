86f371fa69041470b9a1ee1f1441922179ce9752
Chris Larson <krystoff@uw.edu>
2012-02-25 18:52:06 -0800

    Chap12: 2 part email working

--- ticketee/app/models/ticket.rb
+++ ticketee/app/models/ticket.rb
@@ -11,13 +11,20 @@ class Ticket < ActiveRecord::Base
   accepts_nested_attributes_for :assets
   has_many :comments
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers", :class_name => "User"
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
+  after_create :creator_watches_me
   
   def tag!(tags)
     tags = tags.split(" ").map do |tag|
       Tag.find_or_create_by_name(tag)
     end
     self.tags << tags
+  end
+  
+  private
+  def creator_watches_me
+    self.watchers << user
   end
 end
